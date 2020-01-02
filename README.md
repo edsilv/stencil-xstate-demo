0f2fcc1871b9a3029b65dd51220f09fc093554f9
Edward Silverton <e.silverton@gmail.com>
2020-01-02 14:38:24 +0000

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # stencil-xstate-demo
 
 Using [XState](https://xstate.js.org/) with [StencilJS](https://stenciljs.com)
+
+https://stencil-xstate-demo.netlify.com/
--- src/components/counter/counter-machine.ts
+++ src/components/counter/counter-machine.ts
@@ -28,19 +28,9 @@ export interface CounterContext {
   max: number;
 }
 
-// Actions
-// couldn't get any combination of outside functions to work...
-// const increment = context => context.count + 1;
-// const decrement = context => context.count - 1;
-// const increment = assign({ count: context => context.count + 1 });
-// const decrement = assign({ count: context => context.count - 1 });
-
 // Guards
-//const isMax = ctx => ctx.count === ctx.max;
 const isNotMax = ctx => ctx.count < ctx.max;
-//const isMin = ctx => ctx.count === ctx.min;
 const isNotMin = ctx => ctx.count > ctx.min;
-//const isMid = ctx => ctx.count !== ctx.min && ctx.count !== ctx.max;
 
 export const counterMachine = Machine<CounterContext, CounterStateSchema, CounterEvent>({
   id: "count",
